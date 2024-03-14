add8285a8dcb86cc2482bc405c438f8e33c6840b
Nithin-kasturi <nithinkasturi8@gmail.com>
2024-03-14 13:48:12 +0530

    Update package dependencies and modify App.js and global styles

--- src/App.js
+++ src/App.js
@@ -1,94 +1,78 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
-
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import {con} from './contract';
+import Details from "./Details";
 export default function Home() {
+  const [signer,setSigner]=useState("");
+  const [connect,setConnect]=useState("Connect to metamask");
+  const [smartContract,setsmartContract]=useState("");
+  const [name,setName]=useState("");
+  const [message,setMessage]=useState("");
+  const buyMeCoffee=async()=>{
+    try{
+        const done=await smartContract.functions.buyCoffee(message,name,{value:ethers.utils.parseEther("0.01")});
+        const buyMeCoffee=async()=>{
+    try{
+        const transaction=await smartContract.functions.buyCoffee(message,name,{value:ethers.utils.parseEther("0.01")});
+        await transaction.wait();
+        alert("Thank you very much");
+        window.location.reload();
+    }
+    catch(err){
+      console.log(err);
+      alert('Failed to buy coffee');
+      window.location.reload();
+    }
+  }
+    }
+    catch(err){
+      console.log(err);
+      alert('Failed to buy coffee');
+      window.location.reload();
+    }
+  }
+  const connectToMetamask=async()=>{
+    if(!window.ethereum){
+      alert("Please download and use metamask")
+    }
+    try{
+      const provider=new ethers.providers.Web3Provider(window.ethereum,'any');
+      await provider.send("eth_requestAccounts",[]);
+      const signer=provider.getSigner();
+      setSigner(signer);
+      setConnect("Connected");
+      setsmartContract(new ethers.Contract(con.address,con.abi,signer));
+      console.log(await signer.getAddress());
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <main className="main">
-      <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
-
-          <div className="connect">
-            <ConnectWallet />
-          </div>
-        </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+    <div className="h-full w-full flex flex-col items-center pt-5">
+      <div className="">  
+        <button onClick={connectToMetamask} className="bg-white rounded-3xl p-2 text-blue-600 md:w-[500px] hover:bg-blue-600 hover:text-white">{connect}</button>    
       </div>
-    </main>
-  );
+      <div className="h-full w-full bg-white mt-10 rounded-3xl py-5">
+          <div className="flex flex-col">
+            <div className="flex justify-center text-black gap-5 my-5">
+                <label>Name</label>
+                <input type="text" onChange={(e)=>setName(e.target.value)} placeholder="Nithin" className="border-2 border-black rounded-xl text-center"/>
+            </div>
+            <div className="flex justify-center text-black gap-5 my-5">
+                <label>Message</label>
+                <input type='text' placeholder="Message goes here"
+                 onChange={(e)=>setMessage(e.target.value)}
+                 className="border-2 border-black rounded-xl text-center"/>
+            </div>
+            <div className="flex justify-center my-5">
+              <button onClick={buyMeCoffee} className="bg-blue-600 rounded-3xl p-2 text-white w-[250px] hover:bg-black hover:text-white">Buy me a coffee 0.01 ETH</button>
+            </div>
+          </div>
+      </div>
+      <Details/>
+    </div>
+    );
 }
